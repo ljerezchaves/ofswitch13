18a2b2d7e9a301b7153c8affc99ba2dac187eef2
Luciano J Chaves <ljerezchaves@gmail.com>
2015-06-15 22:17:55 -0300

    Setting the max number of queues in datapath.

--- model/ofswitch13-net-device.cc
+++ model/ofswitch13-net-device.cc
@@ -610,7 +610,7 @@ OFSwitch13NetDevice::DatapathNew ()
 
   list_init (&dp->port_list);
   dp->ports_num = 0;
-  dp->max_queues = 0; // No queue support by now
+  dp->max_queues = OFSwitch13Queue::GetMaxQueues ();
   dp->exp = 0;
 
   dp->config.flags = OFPC_FRAG_NORMAL; // IP fragments with no special handling
