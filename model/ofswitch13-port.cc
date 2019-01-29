5a222a0ef27f36563d78356d82c4f1e5573e7e4f
Luciano J Chaves <ljerezchaves@gmail.com>
2019-01-29 18:46:33 -0200

    Small fix on factory create procedure for automatic casting.

--- model/ofswitch13-port.cc
+++ model/ofswitch13-port.cc
@@ -173,7 +173,7 @@ OFSwitch13Port::NotifyConstructionCompleted ()
   memset (m_swPort->queues, 0x00, sizeof (m_swPort->queues));
   m_swPort->max_queues = m_swPort->dp->max_queues;
   m_swPort->num_queues = 0;
-  m_portQueue = DynamicCast<OFSwitch13Queue> (m_factQueue.Create ());
+  m_portQueue = m_factQueue.Create<OFSwitch13Queue> ();
   m_portQueue->SetPortStruct (m_swPort);
   m_portQueue->Initialize ();
   if (csmaDev)
